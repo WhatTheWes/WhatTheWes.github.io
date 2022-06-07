3f06e6b420400015e4d7d35e0d8ef4f9494782b2
WhatTheWes <103788297+WhatTheWes@users.noreply.github.com>
2022-06-07 03:00:27 -0400

    Add files via upload

---
+++ workoutJS.js
@@ -0,0 +1,23 @@
+function myFunction() {
+		var x = document.getElementById("myLinks");
+			if (x.style.display === "block") {
+				x.style.display = "none";
+			} else {
+				x.style.display = "block";
+		}
+	}
+	
+
+function dayOneOn () {
+	document.getElementById("day1").removeAttribute('hidden');
+}
+
+function dayTwoOn () {
+	document.getElementById("day2").removeAttribute('hidden');
+}
+
+function dayThreeOn () {
+	document.getElementById("day3").removeAttribute('hidden');
+}
+
+	
